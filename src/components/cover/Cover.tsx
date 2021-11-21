cef5bcfd330d4d7355cb29209fcc81dfdb150b39
Joselu <jose.gallardo994@gmail.com>
2021-11-21 13:39:45 +0100

    Initial commit with page structure

---
+++ src/components/cover/Cover.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Cover.css";
+
+const Cover = () => {
+  return (
+    <div className="cover-container">
+      <h1>José Luis Gallardo</h1>
+      <p>Developer | Designer | Content Creator | Entrepeneur </p>
+    </div>
+  );
+};
+
+export default Cover;
---
+++ src/components/slider/Slider.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Slider.css";
+
+const Slider = () => {
+  return (
+    <div className="carousel-container">
+      <div className="carousel-title">
+        <h2>My Projects</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Slider;
