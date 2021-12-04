cac80154d9a6248866180a59674cb81904115739
Joselu <joselusw@outlook.com>
2021-12-04 22:24:14 +0100

    Create technology section

--- src/components/cover/Cover.tsx
+++ src/components/cover/Cover.tsx
@@ -3,31 +3,31 @@ import Typewriter from "typewriter-effect";
 import "./Cover.css";
 
 const Cover = () => {
-	const phrases = [
-		".NET developer",
-		"Virgin Angular developer",
-		"Chad React Enjoyer",
-		"Full time freak",
-	];
+  const phrases = [
+    ".NET developer",
+    "Virgin Angular developer",
+    "Chad React Enjoyer",
+    "Full time freak",
+  ];
 
-	return (
-		<div className="cover-container">
-			<img
-				className="avatar"
-				src="https://avatars.githubusercontent.com/u/11831112?v=4"
-			/>
-			<h1>José Luis Gallardo</h1>
-			<Typewriter
-				options={{
-					strings: phrases,
-					autoStart: true,
-					loop: true,
-					cursorClassName: "dynamic-text",
-					wrapperClassName: "dynamic-text",
-				}}
-			/>
-		</div>
-	);
+  return (
+    <div className="cover-container">
+      <img
+        className="avatar"
+        src="https://avatars.githubusercontent.com/u/11831112?v=4"
+      />
+      <h1>José Luis Gallardo</h1>
+      <Typewriter
+        options={{
+          strings: phrases,
+          autoStart: true,
+          loop: true,
+          cursorClassName: "dynamic-text",
+          wrapperClassName: "dynamic-text",
+        }}
+      />
+    </div>
+  );
 };
 
 export default Cover;
